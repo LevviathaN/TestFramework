1ce83ff5ddb76d1ffcba866e16b8d68dada37928
Ruslan <Ruslan@RUSLAN->
2018-10-08 10:51:51 +0300

    First commit

---
+++ TestFramework/utils/BaseTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void startTest()
+        {
+            Browsers.Init();
+        }
+
+        [OneTimeTearDown]
+        public void endTest()
+        {
+            Browsers.Close();
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+            driver.FindElement(By.XPath(".//*[@class='gsfi']")).SendKeys("Test");
+
+        }
+    }
+}
---
+++ TestFramework/utils/Browsers.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework
+{
+    public class Browsers
+    {
+        private static IWebDriver webDriver;
+        public static ReportsManager reports;
+        private static string baseURL = "https://www.google.com/";
+        private static string browser = "Chrome";
+
+        public static void Init()
+        {
+            switch (browser)
+            {
+                case "Chrome":
+                    webDriver = new ChromeDriver();
+                    break;
+//                case "IE":
+//                    webDriver = new InternetExplorerDriver();
+//                    break;
+                case "Firefox":
+                    webDriver = new FirefoxDriver();
+                    break;
+            }
+            webDriver.Manage().Window.Maximize();
+            reports = new ReportsManager(browser, baseURL);
+            Goto(baseURL);
+        }
+
+        public static string Title
+        {
+            get { return webDriver.Title; }
+        }
+
+        public static IWebDriver getDriver
+        {
+            get { return webDriver; }
+        }
+
+        public static void Goto(string url)
+        {
+            webDriver.Url = url;
+            reports.verifyURL(url);
+        }
+
+        public static void Close()
+        {
+            webDriver.Quit();
+        }
+    }
+}
